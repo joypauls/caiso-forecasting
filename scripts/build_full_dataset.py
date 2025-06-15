76b64675be4cf2c037ed9b345f426d6bfbb5d5ab
joypauls <joypaulsen3@gmail.com>
2025-06-14 23:41:15 -0500

    start data loader class

---
+++ scripts/build_full_dataset.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import os
+
+from forecaiso.data_loader import ISODataLoader
+
+DATA_DIR = "../data/hourly_demand/"
--- scripts/create_full_dataset.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-
-DATA_DIR = "../data/hourly_demand/"
